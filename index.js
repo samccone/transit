6159faf722996bddfbea815e889ebb94d98b1ec2
Sam Saccone <samccone@gmail.com>
2014-05-16 19:56:52 -0400

    remove dep on lodash on index

--- index.js
+++ index.js
@@ -3,24 +3,35 @@ var http      = require('http');
 var app       = require("express")()
 var getFeed   = require("./util/get_vehicles");
 var getRoute  = require("./util/get_route");
-var _         = require("lodash");
 
 app.get("/api/feed", function(req, res, next) {
   getFeed()
-  .then(_.bind(res.json, res))
+  .then(res.json.bind(res))
   .catch(next)
 });
 
 app.get("/api/route/:route_id", function(req, res, next) {
   getRoute(req.params['route_id'])
-  .then(_.bind(res.json, res))
+  .then(res.json.bind(res))
   .catch(next);
 });
 
 app.get("/api/feed/near", function(req, res, next) {
   getFeed
   .near(req.query)
-  .then(_.bind(res.json, res))
+  .filter(function(d) {
+    return d.vehicle.trip && d.vehicle.trip.route_id;
+  })
+  .map(function(d) {
+    return getRoute(d.vehicle.trip.route_id)
+    .then(function(route) {
+      d.route = route;
+    })
+    .then(function() {
+      return d;
+    });
+  })
+  .then(res.json.bind(res))
   .catch(next);
 });
 
